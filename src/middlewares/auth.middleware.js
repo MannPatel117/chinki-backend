4ec5354eed1b6ac575f91ba9ef6e5307c232a50b
Mann <mann.patel42@nmims.edu.in>
2024-02-29 15:48:43 +0530

    complete api for AdminUsers

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+import { AdminUser } from "../models/adminUser.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js"
+
+export const verifyJWT = asyncHandler(async(req, res, next) => {
+    try {
+        const token = req.header("Authorization")?.replace("Bearer ", "")
+
+        if (!token) {
+            return res.status(401).json(
+                new ApiResponse(401, "No Token Found", "TOKEN ERROR")
+            )
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await AdminUser.findById(decodedToken?._id).select("-password -refreshToken")
+        if (!user) {
+            return res.status(401).json(
+                new ApiResponse(401, "Invalid Access Token", "TOKEN ERROR")
+            )
+        }
+        req.user = user;
+        next()
+    } catch (error) {
+        return res.status(401).json(
+            new ApiResponse(401, error, "TOKEN ERROR")
+        )
+    }
+    
+})
--- src/routes/adminUser.routes.js
+++ src/routes/adminUser.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { loginAdminUser, registerAdminUser } from "../controllers/adminUser.controller.js";
+import { loginAdminUser, registerAdminUser, getUser, logoutUser, refreshAccessToken } from "../controllers/adminUser.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.route("/login").post(loginAdminUser)
 router.route('/register').post(registerAdminUser)
+router.route('/refreshToken').post(refreshAccessToken)
+router.route('/getUser').get(verifyJWT, getUser)
+router.route('/logout').post(verifyJWT, logoutUser)
+
 
 export default router
