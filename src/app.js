cfeb0c6919d65c9fa5dea5464437cf0e0cb6306d
Mann <mann.patel42@nmims.edu.in>
2024-03-29 16:05:20 +0530

    vercel edits

--- src/app.js
+++ src/app.js
@@ -9,7 +9,9 @@ app.use(cors())
 app.use(express.json({limit:"16kb"}))
 app.use(cookieParser())
 
-
+app.get("/", (req, res) => {
+    res.status(200).send("Hello From Chinki");
+});
 //routes imports
 import adminUserRouter from './routes/adminUser.routes.js'
 import productsRouter from './routes/product.routes.js'
